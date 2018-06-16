d65b1dd86b7c9ebf29871b4807f6f947d1828ad4
Blue Flamingos <illawarra9761@gmail.com>
2018-06-16 15:32:36 +1000

    port values

---
+++ src/main/kotlin/frc/team6434/robot/Ports.kt
@@ -0,0 +1,17 @@
+package frc.team6434.robot
+
+class Ports {
+    val XBOX_CONTROLLER_PORT = 0
+
+    // CAN Bus Node IDs
+    val LEFT_A_CANID = 1
+    val LEFT_B_CANID = 2
+    val RIGHT_A_CANID = 3
+    val RIGHT_B_CANID = 4
+
+    // Lift motor control channels
+    val LIFT_A_CHANNEL = 1
+    val LIFT_B_CHANNEL = 2
+    val LIFT_C_CHANNEL = 3
+    val LIFT_D_CHANNEL = 4
+}

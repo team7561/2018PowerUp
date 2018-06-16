23412cbd9b8dad1f4dcd085ab8f895af4be61b30
Blue Flamingos <illawarra9761@gmail.com>
2018-06-16 15:57:51 +1000

    robot program

--- src/main/kotlin/frc/team6434/robot/Robot.kt
+++ src/main/kotlin/frc/team6434/robot/Robot.kt
@@ -3,60 +3,23 @@ package frc.team6434.robot
 import edu.wpi.first.wpilibj.GenericHID.Hand
 import edu.wpi.first.wpilibj.IterativeRobot
 import edu.wpi.first.wpilibj.XboxController
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import edu.wpi.first.wpilibj.Spark
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+
 
 class Robot : IterativeRobot() {
-    val XBOX_CONTROLLER_PORT = 0
-
-    // CAN Bus Node IDs
-    val LEFT_A_CANID = 1
-    val LEFT_B_CANID = 2
-    val RIGHT_A_CANID = 3
-    val RIGHT_B_CANID = 4
-
-    // Lift motor control channels
-    val LIFT_A_CHANNEL = 1
-    val LIFT_B_CHANNEL = 2
-    val LIFT_C_CHANNEL = 3
-    val LIFT_D_CHANNEL = 4
-
-
-    val CROSS_LINE_DURATION = 2.0
-    val CROSS_LINE_POWER = 0.75
-
-
+ 
     lateinit var controller: XboxController
 
-    lateinit var leftA: WPI_TalonSRX
-    lateinit var leftB: WPI_TalonSRX
-    lateinit var rightA: WPI_TalonSRX
-    lateinit var rightB: WPI_TalonSRX
-
-    lateinit var liftA: Spark
-    lateinit var liftB: Spark
-    lateinit var liftC: Spark
-    lateinit var liftD: Spark
-
+    lateinit var drivetrain: Drivetrain
+    lateinit var lift: Lift
+  
     override fun robotInit() {
         println("Hello Illawarra 9761")
 
         controller = XboxController(XBOX_CONTROLLER_PORT)
 
-        leftA = WPI_TalonSRX(LEFT_A_CANID)
-        leftB = WPI_TalonSRX(LEFT_B_CANID)
-        rightA = WPI_TalonSRX(RIGHT_A_CANID)
-        rightB = WPI_TalonSRX(RIGHT_B_CANID)
-
-        liftA = Spark(LIFT_A_CHANNEL)
-        liftB = Spark(LIFT_B_CHANNEL)
-        liftC = Spark(LIFT_C_CHANNEL)
-        liftD = Spark(LIFT_D_CHANNEL)
-
-
-        leftB.follow(leftA)
-        rightB.follow(rightA)
+        drivetrain = Drivetrain()
+        lift = Lift()
     }
 
     fun clamp(num: Double): Double {
@@ -74,13 +37,9 @@ class Robot : IterativeRobot() {
       return num
     }
 
-    fun getLiftPower(leftBumper: Boolean, rightBumper: Boolean): Double {
-      if (leftBumper)
-        return -1.0
-      if (rightBumper)
-        return 1.0
-      return 0.0
-    }
+          fun getLiftPower(leftBumper: Boolean, rightBumper: Boolean): Double {
+           
+          }
 
 
     var startTime: Long = 0
@@ -96,10 +55,7 @@ class Robot : IterativeRobot() {
 
 
       var drivePower = if (elapsedTime < CROSS_LINE_DURATION) CROSS_LINE_POWER else 0.0
-      SmartDashboard.putNumber("leftPower", drivePower)
-      SmartDashboard.putNumber("rightPower", drivePower)
-      leftA.set(drivePower)
-      rightA.set(drivePower)
+      drivetrain.setPower(drivePower, drivePower) 
     }
 
 
@@ -117,16 +73,14 @@ class Robot : IterativeRobot() {
 
       var leftPower = clamp(y + orx)
       var rightPower = clamp(y - orx)
-      SmartDashboard.putNumber("leftPower", leftPower)
-      SmartDashboard.putNumber("rightPower", rightPower)
-      leftA.set(leftPower)
-      rightA.set(rightPower)
+      drivetrain.setPower(leftPower, rightPower)
 
-      var liftPower = getLiftPower(controller.getBumper(Hand.kLeft), controller.getBumper(Hand.kRight))
-      SmartDashboard.putNumber("liftPower", liftPower)
-      liftA.set(liftPower)
-      liftB.set(liftPower)
-      liftC.set(-liftPower)
-      liftD.set(-liftPower)
+
+      if (leftBumper)
+        lift.raise()
+      else if (rightBumper)
+        lift.lower()
+      else
+        lift.stop()     
     }
 }

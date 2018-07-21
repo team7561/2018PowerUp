a948fa7e025bcff29dd3838857a42a7339401cc7
Blue Flamingos <illawarra9761@gmail.com>
2018-07-21 16:30:10 +1000

    added servo hold and release

--- src/main/kotlin/frc/team9761/robot/Ports.kt
+++ src/main/kotlin/frc/team9761/robot/Ports.kt
@@ -14,7 +14,8 @@ object Ports {
     val DRIVE_RIGHT_B_CHANNEL = 2
     val DRIVE_LEFT_A_CHANNEL = 3
     val DRIVE_LEFT_B_CHANNEL = 4
-    val WRIST_CHANNEL = 5
+    val WRIST_MOTOR_CHANNEL = 5
     val INTAKE_LEFT_CHANNEL = 6
     val INTAKE_RIGHT_CHANNEL = 7
+    val WRIST_SERVO_CHANNEL = 8
 }
--- src/main/kotlin/frc/team9761/robot/Wrist.kt
+++ src/main/kotlin/frc/team9761/robot/Wrist.kt
@@ -3,9 +3,20 @@ package frc.team9761.robot
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.VictorSP
+import edu.wpi.first.wpilibj.Servo
+
 class Wrist {
-      val motor = VictorSP(Ports.WRIST_CHANNEL)
-      
+      val motor = VictorSP(Ports.WRIST_MOTOR_CHANNEL)
+      val servo = Servo(Ports.WRIST_SERVO_CHANNEL)
+
+      fun release() {
+        setservoPosition(1.0)
+      }
+
+      fun hold() {
+        setservoPosition(0.0)
+      }
+
       fun raise() {
         setWristPower(Speeds.WRIST_RAISE_POWER)
       }
@@ -18,10 +29,14 @@ class Wrist {
         setWristPower(0.0)
       }
 
+      fun setservoPosition(position: Double) {
+        SmartDashboard.putNumber("servo", position)
+        motor.set(position)
+      }
+
       fun setWristPower(wristPower: Double) {
         SmartDashboard.putNumber("wristPower", wristPower)
         motor.set(wristPower)
-        
       }
 
       
